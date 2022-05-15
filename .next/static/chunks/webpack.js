2df4f19a6bb58422d4ee9912550053c4a1dd2524
Honey-9 <harshsingh9667903022@gmail.com>
2022-05-15 14:16:21 +0530

    title

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "6828c90efd6e30176aa5";
+/******/ 	var hotCurrentHash = "e6750176a51372232254";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
 	const resumeRef = useRef();
 	const handlePrint = useReactToPrint({
 		content: () => resumeRef.current,
-		documentTitle: 'Vishwajeet Raj',
+		documentTitle: 'Honey',
 	});
 
 	const dispatch = useDispatch();
@@ -33,7 +33,7 @@ export default function Home() {
 		<Fragment>
 		<Head>
         {/* <title>{username ? username : ''} | Resume Editor</title> */}
-        <title>OS Resume | Edit Resume like a Pro.</title>
+        <title>Resumesy.</title>
 		<meta property="og:title" content="OS Resume: Online Resume Builder – Free Download"></meta>
 		<meta property="og:description" content="The best free online resume builder that’ll land you interviews. Create a professional resume in minutes. Download or print your resume for free."></meta>
 		<meta property="og:type" content="website"></meta>
